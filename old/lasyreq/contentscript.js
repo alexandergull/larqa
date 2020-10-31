8e701aaa8530c20a74bd4b1798ca2724ce17946b
alexandergull <galyshev@cleantalk.org>
2020-10-31 15:40:34 +0500

    removed OLD folder

--- old/lasyreq/contentscript.js
+++
@@ -1,7 +0,0 @@
-chrome.extension.onRequest.addListener(
-    function(request, sender, sendResponse) {
-        if(request.method == "getHTML"){
-            sendResponse({html: document.all[0].outerHTML});
-        }
-    }
-);
